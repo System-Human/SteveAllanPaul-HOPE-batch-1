e2c8b324d9bc5bee594fd0170c16ce224276b816
System-Human <steveallan.space@gmail.com>
2026-04-06 09:03:21 +0530

    Delete News Aggerator/com/news directory

--- News Aggerator/com/news/model/FakeNews.java
+++
@@ -1,9 +0,0 @@
-package com.news.model;
-
-public class FakeNews {
-    public String content;
-
-    public FakeNews(String content) {
-        this.content = content;
-    }
-}
--- News Aggerator/com/news/processor/ContentCreator.java
+++
@@ -1,9 +0,0 @@
-package com.news.processor;
-
-import com.news.model.FakeNews;
-
-public class ContentCreator {
-    public FakeNews createSillyStory() {
-        return new FakeNews("Local cat elected as Mayor of New York!");
-    }
-}
